1f2b8fbcf9164b86ff99c4ae9355279d889818a3
SunnyDay <1246771571@qq.com>
2021-12-06 21:32:37 +0800

    change below： 1、gone margin 2、百分比 3、角度约束

--- app/src/main/java/com/sunnyday/constraintlayout/practice/MainActivity.kt
+++ app/src/main/java/com/sunnyday/constraintlayout/practice/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.test6)
+        setContentView(R.layout.test8)
     }
 }
