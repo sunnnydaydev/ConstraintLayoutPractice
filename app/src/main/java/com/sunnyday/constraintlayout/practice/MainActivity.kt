07283bbfe6a26771d6b3d8c6408cfedd227a7c31
SunnyDay <1246771571@qq.com>
2021-12-26 11:55:45 +0800

    change below： add “chain”约束

--- app/src/main/java/com/sunnyday/constraintlayout/practice/MainActivity.kt
+++ app/src/main/java/com/sunnyday/constraintlayout/practice/MainActivity.kt
@@ -12,7 +12,7 @@ const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.test12)
+        setContentView(R.layout.ratio)
        // test10()
     }
 
